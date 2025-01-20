7bb49bb36be7475d288a4e088ad14dd262cf70f0
Gabriela Osorio <g.osorio1085@gmail.com>
2025-01-20 08:30:18 -0400

    configuracion git page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gosorio1085.github.io",
+  base: "portfolio",
   integrations: [tailwind()],
 });
